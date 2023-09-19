459fa28eef4785913dc14bc4d7282d33eae51e39
JeremyFlorman <120654181+JeremyFlorman@users.noreply.github.com>
2023-09-19 10:33:27 -0400

    Delete Intestinal_Calcium_app directory

--- Intestinal_Calcium_app/BatchMergeWormData.m
+++
@@ -1,9 +0,0 @@
-folder  = 'X:\Calcium Imaging\Intestinal_Calcium\DMP_Mutants\';
-
-d = dir(folder);
-d = d(3:end);
-
-for i = 22:length(d)
-    datafolder = fullfile(d(i).folder,d(i).name);
-    combineWormdata(datafolder)
-end
