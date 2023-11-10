631d7a991308ab86727991440b1ac5e767ba559c
JeremyFlorman <jeremy.florman@gmail.com>
2023-11-10 13:46:59 -0500

    copied from OAS repo

--- OAS/H5toTiff.m
+++ OAS/H5toTiff.m
@@ -2,7 +2,7 @@
 % of the 30 seconds preceeding each calcium spike. This is useful for 
 % counting pumping rate. 
 
-d = dir('Z:\OAS\5-HT\ser-4+5HT');
+d = dir('Z:\OAS\5-HT\-Food\eat-2-noFood');
 dirflag= [d(:).isdir];
 d = d(dirflag);
 d = d(3:end);
@@ -28,7 +28,7 @@ wormdata = fullfile(wd(1).folder, wd(1).name);
 %%
 timePre = settings.framerate*30;
 
-locs = mtdata.peakLoc(2:end);
+locs = mtdata.peakLoc(1:end);
 ints = mtdata.peakIntervals;
 img =  [];
 tic
@@ -45,14 +45,31 @@ end
 toc
 %%
 
+if isempty(locs)
+locs = floor(length(img/2));
+end
 
 for i = 1:length(locs)
-    outputFileName = [strrep(wd.name, 'wormdata.mat', ['bfImage_' num2str(i) '_interval(' num2str(round(ints(i))) ').tiff'])];
+    if i == 1
+        interval = 'NA';
+    else
+        interval = num2str(round(ints(i-1)));
+    end
+
+    outputFileName = [strrep(wd.name, 'wormdata.mat', ['bfImage_' num2str(i) '_interval(' interval ').tiff'])];
     localPath = ['C:\tmp\' outputFileName]
     destinationPath = [folder '\' outputFileName];
 
     temploc = locs(i);
-    for k = temploc-timePre:temploc
+    if temploc-timePre <1
+        starttime = 1;
+    else 
+        starttime = temploc-timePre;
+    end
+
+    endtime = starttime+timePre;
+
+    for k = starttime:endtime
         imwrite(img(:, :, k), localPath, 'WriteMode', 'append','Compression','none');
     end
 
