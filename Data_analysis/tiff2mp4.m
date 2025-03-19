b3775872b654f7c0ddab3934408f3a7f64b22892
JeremyFlorman <jeremy.florman@gmail.com>
2025-03-19 16:31:40 -0400

    upload

---
+++ Data_analysis/tiff2mp4.m
@@ -0,0 +1,35 @@
+
+ filename = 'C:\Users\Jeremy\Desktop\230330_zfex813_wt_2_MMStack_Default.ome.tif';
+
+[fp, name, ext] = fileparts(filename);
+info = imfinfo(filename);
+
+fr = 15;    % frame rate of original video 
+playrate = 2; % multiplier for playback framerate
+
+startidx = 1 %460;
+endidx =4500 % 3638 %startidx+(30*fr);
+
+
+
+
+outpath = fullfile(fp, [name '_' num2str(floor(startidx/15)) '-'...
+    num2str(floor(endidx/fr)) '.mp4']);
+
+% info = imfinfo(filename)
+vW = VideoWriter(outpath, 'MPEG-4');
+vW.Quality = 100;
+vW.FrameRate = fr*playrate;
+
+open(vW) 
+figure()
+
+
+for i = startidx:endidx
+     img = uint8(imread(filename,i));
+      imshow(img);
+     writeVideo(vW,img)
+end
+
+ close(vW)
+disp('DONE!!!')
