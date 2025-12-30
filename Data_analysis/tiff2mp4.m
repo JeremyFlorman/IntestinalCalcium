0fa2f7befd4e4e106719190dcd4d54a5b78f5c73
JeremyFlorman <jeremy.florman@gmail.com>
2025-12-30 12:53:06 -0500

    misc

--- Data_analysis/tiff2mp4.m
+++ Data_analysis/tiff2mp4.m
@@ -1,35 +1,40 @@
+% fp = 'C:\Users\Jeremy\Dropbox\ins-3 intestinal calcium fig\movies\'
+% 
+% fn = {[fp '211105_zfis178_wildtype-30mM-TA_3.tif'], [fp '211108_zfis178_wildtype-control_2.tif'], ...
+%     [fp '220524_zfis178_tyra-3-30mM-TA_3.tif'],[fp '211110_zfis178_tyra-3-30mM-TA_2.tif'], ...
+%     [fp '220525_zfis178_itr-1-30mM-TA_4.tif'], [fp '240507_zfis178_itr-1-30mM-TA_4.tif'],[fp '240913_zfis178_wildtype-30mM-TA_3-noFood.tif']};
+% % 
+% for f = length(fn)
+%     filename = fn{f};
+    filename = "C:\Users\Jeremy\Desktop\251110_zfis178_wildtype+Food_4_rgb_croppped.tif";
+    [fp, name, ext] = fileparts(filename);
+    info = imfinfo(filename);
 
- filename = "C:\Users\Jeremy\Desktop\250723_zfis178_wildtype-multipatch\250723_zfis178_wildtype-multipatch_0-8min_composite.tif";
+    fr = 15;    % frame rate of original video
+    playrate = 1; % multiplier for playback framerate
 
-[fp, name, ext] = fileparts(filename);
-info = imfinfo(filename);
-
-fr = 15;    % frame rate of original video 
-playrate = 2; % multiplier for playback framerate
-
-startidx = 1 %460;
-endidx =7200% 3638 %startidx+(30*fr);
-
-
-
-
-outpath = fullfile(fp, [name '_' num2str(floor(startidx/15)) '-'...
-    num2str(floor(endidx/fr)) '.mp4']);
-
-% info = imfinfo(filename)
-vW = VideoWriter(outpath, 'MPEG-4');
-vW.Quality = 100;
-vW.FrameRate = fr*playrate;
-
-open(vW) 
-figure()
+    startidx = 1; %460;
+    endidx = length(info);
 
 
-for i = startidx:endidx
-     img = uint8(imread(filename,i));
-      imshow(img);
-     writeVideo(vW,img)
-end
 
- close(vW)
-disp('DONE!!!')
+    outpath = strrep(filename, '.tif',['_' num2str(playrate) 'x_Speed.mp4']);
+
+    % info = imfinfo(filename)
+    vW = VideoWriter(outpath, 'MPEG-4');
+    vW.Quality = 100;
+    vW.FrameRate = fr*playrate;
+
+    open(vW)
+    
+
+
+    for i = startidx:endidx
+        img = uint8(imread(filename,i));
+%         imshow(img);
+        writeVideo(vW,img)
+    end
+
+    close(vW)
+    disp('DONE!!!')
+% end
