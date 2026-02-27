26fda13191e612a2a1f76ffe5ab6cf60feee9277
JeremyFlorman <jeremy.florman@gmail.com>
2026-02-27 13:15:34 -0500

    pumping rate can be added via kymograph approach

--- Data_analysis/addBehaviorAnnotations.m
+++ Data_analysis/addBehaviorAnnotations.m
@@ -45,9 +45,7 @@ if ~isempty(sheets)
         data = readtable(fullfile(sheets(i).folder,sheets(i).name));
         events = data{:,'Name'};
         pumpFrames = cellfun(@str2double, regexp(events,"\d*(?=-)","match", 'once'));
-
         wormDataFile = dir([sheets(i).folder '\*wormdata.mat']);
-
         load(fullfile(wormDataFile(1).folder, wormDataFile(1).name))
 
 
@@ -55,14 +53,6 @@ if ~isempty(sheets)
         numFrames = length(wormdata.bulkSignal);
         pumpingRate = zeros(numFrames,1);
 
-        % for k = fps:numFrames
-        %     lo = k-fps;
-        %     hi = k;
-        %     pumpsInWindow = pumpFrames(pumpFrames>lo & pumpFrames < hi);
-        %     if ~isempty(pumpsInWindow)
-        %         pumpingRate(k) = numel(pumpsInWindow);
-        %     end
-        % end
         for k = 1:numFrames
             lo = max(1, k - fps + 1);
             hi = k;
@@ -72,10 +62,7 @@ if ~isempty(sheets)
             pumpingRate(k) = numel(pumpsInWindow) / windowSec;
         end
 
-
-
         smoothedPumping = smoothdata(pumpingRate, "movmean", 30);
-
 
         if strcmp(wormDataFile.name, '260128_zfis178_wildtype-noFood-0min_7_wormdata.mat')
             pumpingRate(6200:end) = nan;
@@ -89,4 +76,44 @@ if ~isempty(sheets)
         plot(smoothedPumping)
     end
 end
+
+sheets = dir([fp '\**\*pumpingKymo*.csv']);
+if ~isempty(sheets)
+    for i = 1:length(sheets)
+        data = readtable(fullfile(sheets(i).folder,sheets(i).name));
+        events = data{:,'X'};
+        pumpFrames = sort(events, 'ascend');
+        wormDataFile = dir([sheets(i).folder '\*wormdata.mat']);
+        load(fullfile(wormDataFile(1).folder, wormDataFile(1).name))
+
+        pumpFrames = pumpFrames+wormdata.stimTimes(1)-(30*fps); % correct for unanalyzed video
+
+        %% Instantaneous frequency trace
+        numFrames = length(wormdata.bulkSignal);
+        pumpingRate = zeros(numFrames,1);
+
+        for k = 1:numFrames
+            lo = max(1, k - fps + 1);
+            hi = k;
+            windowSec = (hi - lo + 1)/fps;
+
+            pumpsInWindow = pumpFrames(pumpFrames >= lo & pumpFrames <= hi);
+            pumpingRate(k) = numel(pumpsInWindow) / windowSec;
+        end
+
+        smoothedPumping = smoothdata(pumpingRate, "movmean", 30);
+
+        if strcmp(wormDataFile.name, '260128_zfis178_wildtype-noFood-0min_7_wormdata.mat')
+            pumpingRate(6200:end) = nan;
+        end
+
+        wormdata.pumpTimes = pumpFrames;
+        wormdata.pumpingRate = pumpingRate;
+
+        save(fullfile(wormDataFile(1).folder, wormDataFile(1).name), 'wormdata');
+        figure()
+        plot(smoothedPumping)
+    end
+end
+
 end
